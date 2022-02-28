dc4702bf568bf29cfd1a705625340983fd29ddc6
adlerfl <florian.adler@ext.ec.europa.eu>
2022-02-28 17:07:01 +0100

    deploy graphql API app

--- APPS/MP-GENERATED/REST-GRAGHQL_API/deploy.sh
+++ APPS/MP-GENERATED/REST-GRAGHQL_API/deploy.sh
@@ -1,3 +1,3 @@
 . ~/DEV/set-env.sh
 mvn clean package -Ptomcat-embedded-ds
-cp ./REST/target/scoreKendoUiApp.war ~/DEV/GITHUB/fmadler/MicroServices/MicroServices/game_score/APPS/__RELEASES/tomcat
+cp ./REST/target/scoreKendoUiApp.war ../../APPS/__RELEASES/tomcat
