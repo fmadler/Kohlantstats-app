23615ede8b791e7819eb1ae673fae0a2a29de952
adlerfl <florian.adler@ext.ec.europa.eu>
2023-03-31 22:46:39 +0200

    kls le feu sacré encodage

--- MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.5/V0.8.5.8__2023__team_game_camp.sql
+++ MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.5/V0.8.5.8__2023__team_game_camp.sql
@@ -1,100 +1,81 @@
-call create_game('le-feu-sacre','Faire du feu','faire-du-feu-2023','faire-du-feu','individuel','capitaine',7,10,'capitaine,talisman,elimination-directe','rapidite,dexterite');
+INSERT INTO `GS_REWARD` (`NAME`, `WEB_PATH`, `GS_REWARD_TYPE_ID`, `IS_POSITIVE`) VALUES ('Poulets rotis', 'poulets-rotis', 5, 1);
+call create_game('le-feu-sacre','Régate','regate-2023','regate','collectif','confort',8,14,'poulets-rotis','rapidite,force,navigation,construction');
 
 
-14ème jour regate victoire rouge (confort)
-15 ème abandon benjamin
-16 ème tania revient jaune-2
+call create_game_participation('helena-2023','rouge-2-2023','regate-2023','participant','collectif',1,'poulets-rotis');
+call create_game_participation('anne-sophie-2023','rouge-2-2023','regate-2023','participant','collectif',1,'poulets-rotis');
+call create_game_participation('esteban-2023','rouge-2-2023','regate-2023','participant','collectif',1,'poulets-rotis');
+call create_game_participation('quentin-2023','rouge-2-2023','regate-2023','participant','collectif',1,'poulets-rotis');
+call create_game_participation('rudy-2023','rouge-2-2023','regate-2023','participant','collectif',1,'poulets-rotis');
+call create_game_participation('frederic-2023','rouge-2-2023','regate-2023','participant','collectif',1,'poulets-rotis');
+call create_game_participation('clemence-2023','rouge-2-2023','regate-2023','participant','collectif',1,'poulets-rotis');
+
+call create_game_participation('nicolas-2023','jaune-2-2023','regate-2023','participant','collectif',2,'');
+call create_game_participation('grace-2023','jaune-2-2023','regate-2023','participant','collectif',2,'');
+call create_game_participation('laura-2023','jaune-2-2023','regate-2023','participant','collectif',2,'');
+call create_game_participation('gilles-2023','jaune-2-2023','regate-2023','participant','collectif',2,'');
+call create_game_participation('benjamin-2023','jaune-2-2023','regate-2023','participant','collectif',2,'');
+call create_game_participation('martin-2023','jaune-2-2023','regate-2023','participant','collectif',2,'');
+call create_game_participation('julie-2023','jaune-2-2023','regate-2023','participant','collectif',2,'');
+call create_game_participation('christine-2023','jaune-2-2023','regate-2023','participant','collectif',2,'');
 
 
-16 ème tour poids placement-de-sacs-sur-tour épreuve (immunite)
+call team_game_ranking ('jaune-2-2023','regate-2023',2);
+call team_game_ranking ('rouge-2-2023','regate-2023',1);
 
-rouge esteban exclus-tirage-sort
-jaune christine laura exclus
+-- 14ème jour regate victoire rouge (confort)
+-- 15 ème abandon benjamin
+-- 16 ème tania revient jaune-2
 
-victoire jaune
+call create_participant_departure ('le-feu-sacre','benjamin-2023','jaune-2-2023',15,'abandon-volantaire', @participant_team_id);
+call fill_participant_longevity ('le-feu-sacre','benjamin-2023',15,1,15);
 
-vote
+call create_participant_entrance ('le-feu-sacre','tania-2023','jaune-2-2023',16,'remplacement', @participant_team_id);
 
-esteban -> fred
-anne sophie -> rudy
-helena -> rudy
-quentin -> rudy
-clemence -> rudy
-rudy -> anne-sophie
-fred -> rudy
+INSERT INTO `GS_GAME_TYPE` (`NAME`, `WEB_PATH`, `DESCRIPTION`) VALUES ('Placement de sacs sur une tour', 'placement-de-sacs-sur-tour', NULL);
 
-fred donne le talisman à quentin
+call create_game('le-feu-sacre','placement-de-sacs-sur-tour','placement-de-sacs-sur-tour-2023','placement-de-sacs-sur-tour','collectif','immunite',9,16,'totem','rapidite,precision,coordination');
 
+call create_game_participation('helena-2023','rouge-2-2023','placement-de-sacs-sur-tour-2023','participant','collectif',1,'');
+call create_game_participation('anne-sophie-2023','rouge-2-2023','placement-de-sacs-sur-tour-2023','participant','collectif',1,'');
+call create_game_participation('esteban-2023','rouge-2-2023','placement-de-sacs-sur-tour-2023','exclusion-apres-tirage-au-sort','collectif',1,'');
+call create_game_participation('quentin-2023','rouge-2-2023','placement-de-sacs-sur-tour-2023','participant','collectif',1,'');
+call create_game_participation('rudy-2023','rouge-2-2023','placement-de-sacs-sur-tour-2023','participant','collectif',1,'');
+call create_game_participation('frederic-2023','rouge-2-2023','placement-de-sacs-sur-tour-2023','participant','collectif',1,'');
+call create_game_participation('clemence-2023','rouge-2-2023','placement-de-sacs-sur-tour-2023','participant','collectif',1,'');
 
+call create_game_participation('nicolas-2023','jaune-2-2023','placement-de-sacs-sur-tour-2023','participant','collectif',2,'totem');
+call create_game_participation('grace-2023','jaune-2-2023','placement-de-sacs-sur-tour-2023','participant','collectif',2,'totem');
+call create_game_participation('laura-2023','jaune-2-2023','placement-de-sacs-sur-tour-2023','exclusion-apres-tirage-au-sort','collectif',2,'totem');
+call create_game_participation('gilles-2023','jaune-2-2023','placement-de-sacs-sur-tour-2023','participant','collectif',2,'totem');
+call create_game_participation('tania-2023','jaune-2-2023','placement-de-sacs-sur-tour-2023','participant','collectif',2,'totem');
+call create_game_participation('martin-2023','jaune-2-2023','placement-de-sacs-sur-tour-2023','participant','collectif',2,'totem');
+call create_game_participation('julie-2023','jaune-2-2023','placement-de-sacs-sur-tour-2023','participant','collectif',2,'totem');
+call create_game_participation('christine-2023','jaune-2-2023','placement-de-sacs-sur-tour-2023','exclusion-apres-tirage-au-sort','collectif',2,'totem');
 
-
-
-
-call create_game_participation('helena-2023','rouge-2023','marcher-sur-l-eau-2023','participant','individuel',11,'');
-call create_game_participation('julie-2023','rouge-2023','marcher-sur-l-eau-2023','participant','individuel',13,'');
-call create_game_participation('christine-2023','rouge-2023','marcher-sur-l-eau-2023','participant','individuel',16,'');
-call create_game_participation('anne-sophie-2023','rouge-2023','marcher-sur-l-eau-2023','participant','individuel',9,'');
-call create_game_participation('elodie-2023','rouge-2023','marcher-sur-l-eau-2023','participant','individuel',17,'elimination-directe');
-call create_game_participation('martin-2023','rouge-2023','marcher-sur-l-eau-2023','participant','individuel',10,'');
-call create_game_participation('esteban-2023','rouge-2023','marcher-sur-l-eau-2023','participant','individuel',14,'');
-call create_game_participation('rudy-2023','rouge-2023','marcher-sur-l-eau-2023','participant','individuel',15,'');
-
-call create_game_participation('nicolas-2023','jaune-2023','marcher-sur-l-eau-2023','participant','individuel',5,'');
-call create_game_participation('grace-2023','jaune-2023','marcher-sur-l-eau-2023','participant','individuel',4,'');
-call create_game_participation('frederic-2023','jaune-2023','marcher-sur-l-eau-2023','participant','individuel',7,'');
-call create_game_participation('quentin-2023','jaune-2023','marcher-sur-l-eau-2023','participant','individuel',12,'');
-call create_game_participation('gilles-2023','jaune-2023','marcher-sur-l-eau-2023','exclusion-apres-tirage-au-sort','individuel',1,'capitaine');
-call create_game_participation('tania-2023','jaune-2023','marcher-sur-l-eau-2023','participant','individuel',3,'');
-call create_game_participation('clemence-2023','jaune-2023','marcher-sur-l-eau-2023','participant','individuel',2,'');
-call create_game_participation('benjamin-2023','jaune-2023','marcher-sur-l-eau-2023','participant','individuel',7,'');
-call create_game_participation('celia-2023','jaune-2023','marcher-sur-l-eau-2023','participant','individuel',1,'');
-call create_game_participation('laura-2023','jaune-2023','marcher-sur-l-eau-2023','participant','individuel',6,'');
-
-call create_participant_departure('le-feu-sacre','elodie-2023','rouge-2023',10,'elimination-directe', @participant_team_id);
-call fill_participant_longevity ('le-feu-sacre','elodie-2023',17,1,9);
-
--- fin des equipe rouge-2023 et jaune-2023
+-- 16 ème tour poids placement-de-sacs-sur-tour (immunite)
 
 
+call team_game_ranking ('jaune-2-2023','placement-de-sacs-sur-tour-2023',1);
+call team_game_ranking ('rouge-2-2023','placement-de-sacs-sur-tour-2023',2);
+-- rouge esteban exclus-tirage-sort
+-- jaune christine laura exclus
+
+-- victoire jaune
+
+call first_vote_against ('le-feu-sacre','helena-2023','rudy-2023','rouge-2-2023', 9, 'effectif');
+call first_vote_against ('le-feu-sacre','quentin-2023','rudy-2023','rouge-2-2023', 9, 'effectif');
+call first_vote_against ('le-feu-sacre','anne-sophie-2023','rudy-2023','rouge-2-2023', 9, 'effectif');
+call first_vote_against ('le-feu-sacre','frederic-2023','rudy-2023','rouge-2-2023', 9, 'effectif');
+call first_vote_against ('le-feu-sacre','clemence-2023','rudy-2023','rouge-2-2023', 9, 'effectif');
+call first_vote_against ('le-feu-sacre','esteban-2023','frederic-2023','rouge-2-2023', 9, 'effectif');
+call first_vote_against ('le-feu-sacre','rudy-2023','anne-sophie-2023','rouge-2-2023', 9, 'effectif');
+
+-- fred donne le talisman à quentin
 
 
---
-call create_game('le-feu-sacre','Relais aquatique','relais-aquatique-2023','pierre-apnee','individuel','immunite',8,11,'totem','rapidite,apnee,natation');
 
-call create_game_participation('helena-2023','rouge-2-2023','relais-aquatique-2023','participant','collectif',2,'totem');
-call create_game_participation('anne-sophie-2023','rouge-2-2023','relais-aquatique-2023','participant','collectif',2,'totem');
-call create_game_participation('esteban-2023','rouge-2-2023','relais-aquatique-2023','participant','collectif',2,'totem');
-call create_game_participation('quentin-2023','jaune-2-2023','relais-aquatique-2023','participant','collectif',1,'');
-call create_game_participation('rudy-2023','jaune-2-2023','relais-aquatique-2023','participant','collectif',2,'totem');
-call create_game_participation('frederic-2023','jaune-2-2023','relais-aquatique-2023','exclusion-apres-tirage-au-sort','collectif',1,'');
-call create_game_participation('tania-2023','jaune-2-2023','relais-aquatique-2023','participant','collectif',1,'');
-call create_game_participation('clemence-2023','jaune-2-2023','relais-aquatique-2023','participant','collectif',1,'');
+call create_participant_departure('le-feu-sacre','rudy-2023','rouge-2-2023',9,'exclusion-par-vote', @participant_team_id);
+call fill_participant_longevity ('le-feu-sacre','rudy-2023',14,1,16);
 
-call create_game_participation('nicolas-2023','jaune-2-2023','relais-aquatique-2023','participant','collectif',1,'');
-call create_game_participation('grace-2023','jaune-2-2023','relais-aquatique-2023','participant','collectif',1,'');
-call create_game_participation('laura-2023','jaune-2-2023','relais-aquatique-2023','participant','collectif',1,'');
-call create_game_participation('gilles-2023','jaune-2-2023','relais-aquatique-2023','participant','collectif',1,'');
-call create_game_participation('benjamin-2023','jaune-2-2023','relais-aquatique-2023','participant','collectif',1,'');
-call create_game_participation('martin-2023','jaune-2-2023','relais-aquatique-2023','participant','collectif',2,'totem');
-call create_game_participation('julie-2023','jaune-2-2023','relais-aquatique-2023','participant','collectif',2,'totem');
-call create_game_participation('christine-2023','jaune-2-2023','relais-aquatique-2023','participant','collectif',2,'totem');
-
-call team_game_ranking ('jaune-2-2023','relais-aquatique-2023',1);
-call team_game_ranking ('rouge-2-2023','relais-aquatique-2023',2);
-
-
--- depart de tania
-call first_vote_against ('le-feu-sacre','alexandre-2023','rudy-2023','rouge-2-2023', 9, 'effectif');
-call first_vote_against ('le-feu-sacre','helena-2023','alexandre-2023','rouge-2-2023', 9, 'effectif');
-call first_vote_against ('le-feu-sacre','julie-2023','elodie-2023','rouge-2-2023', 9, 'effectif');
-call first_vote_against ('le-feu-sacre','christine-2023','elodie-2023','rouge-2-2023', 9, 'effectif');
-call first_vote_against ('le-feu-sacre','anne-sophie-2023','alexandre-2023','rouge-2-2023', 9, 'effectif');
-call first_vote_against ('le-feu-sacre','elodie-2023','rudy-2023','rouge-2-2023', 9, 'effectif');
-call first_vote_against ('le-feu-sacre','martin-2023','elodie-2023','rouge-2-2023', 9, 'effectif');
-call first_vote_against ('le-feu-sacre','esteban-2023','alexandre-2023','rouge-2-2023', 9, 'effectif');
-call first_vote_against ('le-feu-sacre','rudy-2023','alexandre-2023','rouge-2-2023', 9, 'effectif');
-
-call create_participant_departure('le-feu-sacre','alexandre-2023','rouge-2-2023',9,'exclusion-par-vote', @participant_team_id);
-call fill_participant_longevity ('le-feu-sacre','alexandre-2023',18,1,9);
-
-call program_current_time ('le-feu-sacre',12);
+call program_current_time ('le-feu-sacre',16);
---
+++ MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.5/V0.8.5.9__2023__team_game_camp.sql
@@ -0,0 +1,81 @@
+call create_game('le-feu-sacre','Perche','perche-2023','perche','collectif','confort',10,16,'nuit-hotel','rapidite,dexterite,collaboration');
+
+
+call create_game_participation('helena-2023','rouge-2-2023','perche-2023','participant','collectif',2,'');
+call create_game_participation('anne-sophie-2023','rouge-2-2023','perche-2023','participant','collectif',2,'');
+call create_game_participation('esteban-2023','rouge-2-2023','perche-2023','participant','collectif',2,'');
+call create_game_participation('quentin-2023','rouge-2-2023','perche-2023','participant','collectif',2,'');
+call create_game_participation('rudy-2023','rouge-2-2023','perche-2023','participant','collectif',2,'');
+call create_game_participation('frederic-2023','rouge-2-2023','perche-2023','participant','collectif',2,'');
+call create_game_participation('clemence-2023','rouge-2-2023','perche-2023','participant','collectif',2,'');
+
+call create_game_participation('nicolas-2023','jaune-2-2023','perche-2023','participant','collectif',1,'nuit-hotel');
+call create_game_participation('grace-2023','jaune-2-2023','perche-2023','participant','collectif',1,'nuit-hotel');
+call create_game_participation('laura-2023','jaune-2-2023','perche-2023','exclusion-apres-tirage-au-sort','collectif',1,'nuit-hotel');
+call create_game_participation('gilles-2023','jaune-2-2023','perche-2023','participant','collectif',1,'nuit-hotel');
+call create_game_participation('martin-2023','jaune-2-2023','perche-2023','participant','collectif',1,'nuit-hotel');
+call create_game_participation('tania-2023','jaune-2-2023','perche-2023','participant','collectif',1,'nuit-hotel');
+call create_game_participation('julie-2023','jaune-2-2023','perche-2023','participant','collectif',1,'nuit-hotel');
+call create_game_participation('christine-2023','jaune-2-2023','perche-2023','exclusion-apres-tirage-au-sort','collectif',1,'nuit-hotel');
+
+
+call team_game_ranking ('jaune-2-2023','perche-2023',1);
+call team_game_ranking ('rouge-2-2023','perche-2023',2);
+
+
+-- 17 ème jour defi placement boule
+INSERT INTO `GS_GAME_STAKE_TYPE` (`NAME`, `WEB_PATH`, `DESCRIPTION`) VALUES ('Talisman', 'talisman', 'Talisman');
+
+call create_game('le-feu-sacre','Empilement boules','empilement-boules-2023','empilement-boules','individuel','talisman',11,17,'nuit-hotel','rapidite,dexterite,collaboration');
+call create_game_participation('nicolas-2023','jaune-2-2023','empilement-boules-2023','participant','collectif',0,'');
+call create_game_participation('grace-2023','jaune-2-2023','empilement-boules-2023','participant','collectif',0,'');
+call create_game_participation('laura-2023','jaune-2-2023','empilement-boules-2023','participant','collectif',0,'');
+call create_game_participation('gilles-2023','jaune-2-2023','empilement-boules-2023','participant','collectif',1,'talisman');
+call create_game_participation('martin-2023','jaune-2-2023','empilement-boules-2023','participant','collectif',0,'');
+call create_game_participation('tania-2023','jaune-2-2023','empilement-boules-2023','participant','collectif',0,'');
+call create_game_participation('julie-2023','jaune-2-2023','empilement-boules-2023','participant','collectif',0,'');
+call create_game_participation('christine-2023','jaune-2-2023','empilement-boules-2023','participant','collectif',0,'');
+
+
+call create_participant_departure ('le-feu-sacre','martin-2023','jaune-2-2023',18,'abandon-medical', @participant_team_id);
+call fill_participant_longevity ('le-feu-sacre','martin-2023',14,1,18);
+
+call create_participant_entrance ('le-feu-sacre','rudy-2023','jaune-2-2023',19,'remplacement', @participant_team_id);
+
+
+call create_game('le-feu-sacre','Catapulte','catapulte-2023','catapulte','collectif','immunite',12,18,'totem,conseil-immediat','rapidite,precision,coordination');
+
+call create_game_participation('helena-2023','rouge-2-2023','catapulte-2023','participant','collectif',1,'totem');
+call create_game_participation('anne-sophie-2023','rouge-2-2023','catapulte-2023','participant','collectif',1,'totem');
+call create_game_participation('esteban-2023','rouge-2-2023','catapulte-2023','participant','collectif',1,'totem');
+call create_game_participation('quentin-2023','rouge-2-2023','catapulte-2023','exclusion-par-choix-d-equipe','collectif',1,'totem');
+call create_game_participation('rudy-2023','rouge-2-2023','catapulte-2023','participant','collectif',1,'totem');
+call create_game_participation('frederic-2023','rouge-2-2023','catapulte-2023','participant','collectif',1,'totem');
+call create_game_participation('clemence-2023','rouge-2-2023','catapulte-2023','participant','collectif',1,'totem');
+
+call create_game_participation('nicolas-2023','jaune-2-2023','catapulte-2023','participant','collectif',2,'conseil-immediat');
+call create_game_participation('grace-2023','jaune-2-2023','catapulte-2023','exclusion-apres-tirage-au-sort','collectif',2,'conseil-immediat');
+call create_game_participation('laura-2023','jaune-2-2023','catapulte-2023','participant','collectif',2,'conseil-immediat');
+call create_game_participation('gilles-2023','jaune-2-2023','catapulte-2023','participant','collectif',2,'conseil-immediat');
+call create_game_participation('tania-2023','jaune-2-2023','catapulte-2023','participant','collectif',2,'conseil-immediat');
+call create_game_participation('martin-2023','jaune-2-2023','catapulte-2023','participant','collectif',2,'conseil-immediat');
+call create_game_participation('julie-2023','jaune-2-2023','catapulte-2023','exclusion-apres-tirage-au-sort','collectif',2,'conseil-immediat');
+call create_game_participation('christine-2023','jaune-2-2023','catapulte-2023','participant','collectif',2,'conseil-immediat');
+
+
+call team_game_ranking ('jaune-2-2023','catapulte-2023',2);
+call team_game_ranking ('rouge-2-2023','catapulte-2023',1);
+
+call first_vote_against ('le-feu-sacre','nicolas-2023','christine-2023','jaune-2-2023', 9, 'effectif');
+call first_vote_against ('le-feu-sacre','julie-2023','tania-2023','jaune-2-2023', 9, 'effectif');
+call first_vote_against ('le-feu-sacre','christine-2023','tania-2023','jaune-2-2023', 9, 'effectif');
+call first_vote_against ('le-feu-sacre','gilles-2023','christine-2023','jaune-2-2023', 9, 'effectif');
+call first_vote_against ('le-feu-sacre','laura-2023','christine-2023','jaune-2-2023', 9, 'effectif');
+call first_vote_against ('le-feu-sacre','tania-2023','christine-2023','jaune-2-2023', 9, 'effectif');
+call first_vote_against ('le-feu-sacre','grace-2023','christine-2023','jaune-2-2023', 9, 'effectif');
+
+
+call create_participant_departure('le-feu-sacre','christine-2023','jaune-2-2023',9,'exclusion-par-vote', @participant_team_id);
+call fill_participant_longevity ('le-feu-sacre','christine-2023',13,1,18);
+
+call program_current_time ('le-feu-sacre',18);
