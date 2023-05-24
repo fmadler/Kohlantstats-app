224835cdae9653392553dfa77a74cbbc2a7bb296
florian adler <florian.adler@ext.ec.europa.eu>
2023-05-24 14:40:38 +0200

    kls 2023 avant la finale

---
+++ MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.5/V0.8.5.18__2023__duels_epreuves_votes.sql
@@ -0,0 +1,25 @@
+-- tania avantage strategique : indice collier immunite
+
+-- tania decouvre collier 34 j
+-- courrier + nourriture
+
+--  35 j
+
+épreuve: labyrinthe, enjeu totem,vote-contre-soi
+
+1 nicolas totem
+2 laura
+3 fred
+4 tania
+5 julie
+6 quentin vote-contre-soi
+
+
+nico -> laura
+laura -> tania
+quentin -> quentin
+quentin -> tania
+quentin -> tania
+fred -> laura
+julie -> laura
+tania -> laura
