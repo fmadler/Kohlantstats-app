31e0998d4af089e20ba32d114a912451096e7292
fmadler <71846395+fmadler@users.noreply.github.com>
2024-09-09 21:40:40 +0200

    Update V0.8.7.3__2024__team_game_camp.sql add regate

---
+++ MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.7/V0.8.7.3__2024__team_game_camp.sql
@@ -0,0 +1,24 @@
+jour 7
+  
+call create_game('la-tribu-maudite','Régate,'regate-2-2024','regate','collectif','confort',8,7,'nuit-luxueuse,avantage-strategique','construction,rapidite');
+
+call create_game_participation('ugo-2024','rouge-2-2024','regate-2-2024','participant','individuel',1,'');
+call create_game_participation('gustin-2024','rouge-2-2024','regate-2-2024','participant','individuel',1,'');
+call create_game_participation('alexandre-2024','rouge-2-2024','regate-2-2024','participant','individuel',1,'');
+call create_game_participation('maxim-2024','rouge-2-2024','regate-2-2024','participant','individuel',1,'');
+call create_game_participation('cecile-2-2024','rouge-2-2024','regate-2-2024','participant','individuel',1,'');
+call create_game_participation('sarah-2-2024','rouge-2-2024','regate-2-2024','participant','individuel',1,'');
+call create_game_participation('cassandre-2024','rouge-2-2024','regate-2-2024','participant','individuel',1,'');
+call create_game_participation('marie-2024','rouge-2-2024','regate-2-2024','participant','individuel',1,'');
+call create_game_participation('emmanuelle-2024','rouge-2-2024','regate-2-2024','participant','individuel',1,'');
+
+call create_game_participation('frederic-2024','jaune-2-2024','regate-2-2024','participant','individuel',1,'');
+call create_game_participation('jacques-2024','jaune-2-2024','regate-2-2024','participant','individuel',1,'');
+call create_game_participation('fabrice-2024','jaune-2-2024','regate-2-2024','participant','individuel',1,'');
+call create_game_participation('thibault-2024','jaune-2-2024','regate-2-2024','participant','individuel',1,'');
+call create_game_participation('michel-2024','jaune-2-2024','regate-2-2024','participant','individuel',1,'');
+call create_game_participation('charlotte-2024','jaune-2-2024','regate-2-2024','participant','individuel',1,'');
+call create_game_participation('lola-2024','jaune-2-2024','regate-2-2024','participant','individuel',1,'');
+call create_game_participation('sophia-2024','jaune-2-2024','regate-2-2024','participant','individuel',1,'');
+call create_game_participation('ilyesse-2024','jaune-2-2024','regate-2-2024','participant','individuel',1,'');
+
