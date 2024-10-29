2c609b0db472b1a4592ee3f48455d4951e1a458e
fmadler <71846395+fmadler@users.noreply.github.com>
2024-10-29 23:16:38 +0100

    Create V0.8.7.9_binome.sql

---
+++ MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.7/V0.8.7.9_binome.sql
@@ -0,0 +1,33 @@
+# binome
+
+- jour: 
+  - binome
+    - ilyesse-ugo
+    - gustin-charlotte
+    - maxim-cassandre
+    - jacques-cecile-2
+    - sophia-thibault
+  - jeu: guide aveugle
+  - victoire:
+    - ilyesse - ugo
+    - confort: repas sur l ile, soirée, chez l habitant
+
+
+- jour: 29
+  - epreuve : immunite: guide aveugle
+    -victoire: ilyesse-ugo
+
+- jour: 30
+  -vote
+    - ilyesse -> maxim
+    - ugo -> jacques
+    - gustin -> jacques
+    - charlotte -> maxim
+    - maxim -> jacques
+    - cassandre -> jacques 
+    - jacques -> maxim x2
+    - cecile-3 -> maxim 
+    - sophia -> maxim 
+    - thibault -> maxim 
+
+- maxim-cassandre eliminé
