8d8344a9a9d2e096c0e2b18e76a13ce3f895d030
fmadler <71846395+fmadler@users.noreply.github.com>
2024-11-12 23:52:09 +0100

    Update V0.8.7.11__2024_epreuves.sql

--- MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.7/V0.8.7.11__2024_epreuves.sql
+++ MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.7/V0.8.7.11__2024_epreuves.sql
@@ -1,5 +1,41 @@
 
-- jour: xx
+- jour: 35
   - confort: 2 equipes de 4: placement de sac: decouverte village
   - cecile, hugo, thibault, cassandre
-  - charlotte, ilyesse, jacques, sophia
+  - charlotte, ilyesse, jacques, sophia: victoire
+
+- jour: 36
+  -epreuve: etoile, immunite
+  - ilyesse (dernier)
+  - hugo
+  - charlotte
+  - thibault
+  - cecile
+  - sophia
+  - cassandre
+  - jacques
+  - 
+  
+- jour: 36
+  - vote: (collier hugo)
+  - ilyesse -> hugo *2
+  - hugo -> ilyesse
+  - charlotte -> cassandre
+  - thibault -> cassandre
+  - cecile -> ilyesse
+  - sophia -> cassandre
+  - cassandre -> ilyesse
+  - jacques -> cassandre
+
+- 2ème vote
+  - vote: (collier hugo)
+  - ilyesse -> cassandre
+  - hugo -> ilyesse
+  - charlotte -> cassandre
+  - thibault -> cassandre
+  - cecile -> ilyesse
+  - sophia -> cassandre
+  - cassandre -> ilyesse
+  - jacques -> cassandre
+
+- depart cassandre
