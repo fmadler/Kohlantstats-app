005d48ff0034b73792cc11c0b46779522dd90821
fmadler <71846395+fmadler@users.noreply.github.com>
2025-03-11 21:55:14 +0100

    Create V0.8.8.03__2025_la_revanche_des_quatre_terres.sql

---
+++ MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.8/V0.8.8.03__2025_la_revanche_des_quatre_terres.sql
@@ -0,0 +1,53 @@
+
+
+est: silongan : vert
+  maxime
+  gaelle : 34 ain, mère au foyer
+  benoit: 24 ans, saut à la perche
+  céline: 39, commerciale dans le soport, savoie
+  maxime: resml sable communication fédération de peche: 33
+  louise: écogarde 24 ans
+  christian: 63, brocanteur: jura
+  
+ sud: blue timone
+  pierre-marie, chef d'entreprise btp
+  pauline: maitre nageuse: 39: corse du sud
+  jérome: peintre en batiment, 43 pyrénées orietnales
+  adiren: ingénieur: 23: bouche du rhone
+  jessica: 34, restauratrice
+  nais: éducatrice spécialisée: 25, marseille
+  
+
+ouest: orange: kaluran
+  mael, patissier
+  mehdi: chauffeur poids lourds, 27, lot et garonne
+  néomei: 32, cheffe d'entreprise, ille et vilaine
+  romane: 20 nantes coach sportif
+  jeromee: 52, ex-militaire, lande
+  cyinthiana: 39 agent immobilier
+
+nord: ilaga violet
+  allan, 25, docker dunkerque
+  mohamed: 30: éducateur sportif
+  frédéreic: 56: directeur service sport: 77 seine et marne
+  andréa: étudiant, 24
+  claire, 37, scénographe produit
+  joana: 32, gérante salon de beauté, seine saint denis
+
+
+
+  -- elastique: confort: feu (3 allumettes), le riz, le dernier: sur l'ile de l'exil
+
+  sud 1
+  nord 2
+  est 3
+  ouest 4 -> ile de l'exil
+
+  -- destruction de cibles 8: totem
+  sud 1
+  nord 2
+  est 3
+  ouest 4
+  
+  sortie :  cynthiana
+  
