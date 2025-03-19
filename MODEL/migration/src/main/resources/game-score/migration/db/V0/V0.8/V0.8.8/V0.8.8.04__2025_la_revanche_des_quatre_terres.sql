ca616dc6438f43eed949d0ebe90e53348a1dc96b
fmadler <71846395+fmadler@users.noreply.github.com>
2025-03-19 22:24:07 +0100

    Update V0.8.8.04__2025_la_revanche_des_quatre_terres.sql

--- MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.8/V0.8.8.04__2025_la_revanche_des_quatre_terres.sql
+++ MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.8/V0.8.8.04__2025_la_revanche_des_quatre_terres.sql
@@ -1,1 +1,39 @@
+Episode 4
+
+6ème jour:
+
+  medhi remplace alan sorti sur blessure
+
+  9ème jour
+  epreuve cage-bambou:  rapidité, agilité: confort
+    - participe pas:
+      - est: maxime (exempt med)
+      - nord: johanna, mohamed
+      - sud: adrien (exempt med), pauline
+    
+  
+    -victoire:
+      - 1: est: nourriture
+      - 2: sud: nourriture
+      - 3: ouest
+      - 4: nord: ile de l''exil
+
+  jour 10
+  epreuve: puzzle tour : immunité
+    - participe pas:
+      - est: louise
+      - nord: medhi, clair
+      - sud: nais (exempt med), jerome
+  
+
+  - victoire
+        - 1: nord: totem
+      - 2: sud: 
+      - 3: ouest conseil
+      - 4: est: conseil immediat
+
+  - 10 ouest: gaelle quitte par vote
+  - 11 nord: medhi abandon
+  - 11 est: benoit par vote
+  - depart de christian
 
