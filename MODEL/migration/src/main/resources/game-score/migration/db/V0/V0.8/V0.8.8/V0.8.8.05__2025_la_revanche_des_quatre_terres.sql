eee8f2636b0bfb9a1aced392d92bf3d7bb82f0aa
fmadler <71846395+fmadler@users.noreply.github.com>
2025-03-25 23:45:22 +0100

    Create V0.8.8.05__2025_la_revanche_des_quatre_terres.sql

---
+++ MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.8/V0.8.8.05__2025_la_revanche_des_quatre_terres.sql
@@ -0,0 +1,64 @@
+12ème jour
+
+  fin des équipe: puis equipe jaune rouge
+  epreuve cocnfort + choix capitaine+collier d'immunité (reunif, prochain conseil) vote contre, : bambou
+
+   - frederic voix contre
+   - jérome 2 voix contre
+  - mael
+  - gale une voix contre
+  - adrien 
+  - mohamed
+  - joana voix contre
+  - celine
+  - andrea
+  - jessica
+  - noemie
+  - louise
+  - pierre-marie
+  - jerome collier
+  - maxime collier
+  - pauline
+  - claire collier capitaine
+  - nais collier capitaine
+
+
+  jaune (virak)
+    maxime cap
+    louise
+    jerome2
+    pierre-marine
+    pauline
+    jerome
+    claire
+    andrea
+    joana
+  
+
+
+  
+
+  rouge (togawe)
+     nais cap
+     gael
+    celine
+    jessica
+    adrien
+    mohamed
+    noemie
+    fred
+    
+    
+  14ème
+    epreuve charges suspendues
+    victoire jaune
+
+  vote conseil elimination mohamed
+  
+   
+
+
+
+
+
+
