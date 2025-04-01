95e79b031bd5fe3c2ee56a1b932144a64e0d67ea
fmadler <71846395+fmadler@users.noreply.github.com>
2025-04-01 22:49:11 +0200

    Update V0.8.8.06__2025_la_revanche_des_quatre_terres.sql

--- MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.8/V0.8.8.06__2025_la_revanche_des_quatre_terres.sql
+++ MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.8/V0.8.8.06__2025_la_revanche_des_quatre_terres.sql
@@ -1,1 +1,27 @@
+15ème jour
 
+  flambeaux: recompense courrier
+jaune (virak)
+    maxime cap exlus tirage au sort
+    louise
+    jerome2
+    pierre-marine
+    pauline
+    jerome
+    claire
+    andrea
+    joana
+
+  rouge (tugawe)
+     nais cap
+     gaelle
+    celine
+    jessica
+    adrien
+    noemie
+    fred
+    mael
+
+victoire rouge
+
+    
