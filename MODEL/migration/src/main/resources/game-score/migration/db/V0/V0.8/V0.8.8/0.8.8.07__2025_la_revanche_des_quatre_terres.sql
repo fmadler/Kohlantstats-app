dd6123ddce6d8c5aa7b07fd52b80c2eb080da5aa
fmadler <71846395+fmadler@users.noreply.github.com>
2025-04-08 23:22:22 +0200

    Create 0.8.8.07__2025_la_revanche_des_quatre_terres.sql

---
+++ MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.8/0.8.8.07__2025_la_revanche_des_quatre_terres.sql
@@ -0,0 +1,18 @@
+17ème
+ - catapulte 9 cibles: confort: buffet
+  - exclus tirage au sort:
+    - rouge: nais
+    - jaune: jerome
+  - victoire
+    - rouge
+
+18ème
+  - grand-bleu
+  - exclus:
+    - rouge: celine
+    - jaune: pierre-marie
+  - victoire
+    - rouge
+
+- vote :
+  - depart: andréa
