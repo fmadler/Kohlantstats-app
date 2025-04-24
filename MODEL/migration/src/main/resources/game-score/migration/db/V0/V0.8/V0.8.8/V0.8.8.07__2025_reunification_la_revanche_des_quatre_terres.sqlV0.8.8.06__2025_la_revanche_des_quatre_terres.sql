a8116afc9483c8f298c723bfb382f79fac5ada8a
fmadler <71846395+fmadler@users.noreply.github.com>
2025-04-24 21:51:36 +0200

    Create V0.8.8.07__2025_reunification_la_revanche_des_quatre_terres.sqlV0.8.8.06__2025_la_revanche_des_quatre_terres.sql

---
+++ MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.8/V0.8.8.07__2025_reunification_la_revanche_des_quatre_terres.sqlV0.8.8.06__2025_la_revanche_des_quatre_terres.sql
@@ -0,0 +1,13 @@
+jour: 20
+  - epreuve: marcher sur l'eau
+  - rouge exempte : celine
+  - jaune exempte : jerome
+  - victoire rouge
+
+  - ambassadeur
+    - rouge: mael, fred,
+    - jaune: louise, pierre-marie
+
+    - depart maxime
+
+jour 22
