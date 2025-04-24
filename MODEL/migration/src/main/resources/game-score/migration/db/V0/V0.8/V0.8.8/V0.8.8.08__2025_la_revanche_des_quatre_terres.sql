665860cff369bc65f0007b123affab87f47245d9
fmadler <71846395+fmadler@users.noreply.github.com>
2025-04-24 23:32:05 +0200

    Create V0.8.8.08__2025_la_revanche_des_quatre_terres.sql

---
+++ MODEL/migration/src/main/resources/game-score/migration/db/V0/V0.8/V0.8.8/V0.8.8.08__2025_la_revanche_des_quatre_terres.sql
@@ -0,0 +1,19 @@
+23 ème
+- koala: immunité
+  - du dernier au premier
+  - fred (vote-contre)
+  - noemie
+  - joana
+  - gaelle
+  - jessica
+  - nais
+  - pierre-marie
+  - jerome2
+  - adien
+  - celine
+  - claire
+  - mael
+  - jerome 2ème
+  - louise 1er
+
+- elimination adrien
